e16f8b1d187263d35649c101b8af3417b130d4e7
kelvin373-ht <kelvin373.ht@gmail.com>
2020-06-26 00:55:22 +0700

    Create ViewModel for get Data Movie

---
+++ utils/src/main/java/com/algokelvin/catalog/movie/tv/utils/viewmodel/AppsViewModel.kt
@@ -0,0 +1,12 @@
+package com.algokelvin.catalog.movie.tv.utils.viewmodel
+
+import com.algokelvin.catalog.movie.tv.repository.`interface`.MovieTVParameter
+import com.algokelvin.catalog.movie.tv.repository.response.ResponseMovie
+
+class AppsViewModel(override val api: String, override val language: String) : BaseViewModel(), MovieTVParameter {
+    private val responseMovie = ResponseMovie(api, language)
+
+    //TODO: Movie
+    fun getMovieNowPlaying() = responseMovie.getMovieNowPlayingResponse()
+
+}
