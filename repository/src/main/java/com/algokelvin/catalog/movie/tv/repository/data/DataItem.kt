b96ee7fe202e220ead6a5e7c8c2b9ae50725c0b3
kelvin373-ht <kelvin373.ht@gmail.com>
2020-06-26 22:58:36 +0700

    Change Data Item Movie

--- repository/src/main/java/com/algokelvin/catalog/movie/tv/repository/data/DataItem.kt
+++ repository/src/main/java/com/algokelvin/catalog/movie/tv/repository/data/DataItem.kt
@@ -1,9 +1,9 @@
 package com.algokelvin.catalog.movie.tv.repository.data
 
-data class DataMovie(val result: ArrayList<Movie>? = null)
+data class DataMovie(val results: ArrayList<Movie>? = null)
 
 data class Movie(
-    val image: Int? = null,
+    val backdrop_path: String? = null,
     val title: String? = null,
     val rating: Double? = null
 )
